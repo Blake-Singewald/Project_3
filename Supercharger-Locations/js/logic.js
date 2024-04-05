c1739609dedae35a1f7564c08311a0ccae1819bb
James Montgomery <145395275+braxtol@users.noreply.github.com>
2024-04-05 15:13:14 -0700

    added National Parks data array
    Thanks Jesse

--- Supercharger-Locations/js/logic.js
+++ Supercharger-Locations/js/logic.js
@@ -1,3 +1,301 @@
+// NationalParks data
+let NationalParks = [
+    {
+      name: "Acadia National Park",
+      location: "Maine",
+      long_lat: [44.3386, 68.2733]
+    },
+    {
+      name: "American Samoa National Park",
+      location: "American Samoa Territory",
+      long_lat: [14.2579, 170.6860]
+    },
+    {
+      name: "Arches National Park",
+      location: "Utah",
+      long_lat: [38.7331, 109.5925]
+    },
+    {
+      name: "Badlands National Park",
+      location: "South Dakota",
+      long_lat: [43.8554, 102.3397]
+    },
+    {
+      name: "Big Bend National Park",
+      location: "Texas",
+      long_lat: [29.2498, 103.2502]
+    },
+    {
+      name: "Biscayne National Park",
+      location: "Florida",
+      long_lat: [25.4824, 80.2083]
+    },
+    {
+      name: "Black Canyon of the Gunnison National Park",
+      location: "Colorado",
+      long_lat: [38.5754, 107.7416]
+    },
+    {
+      name: "Bryce Canyon National Park",
+      location: "Utah",
+      long_lat: [37.5930, 112.1871]
+    },
+    {
+      name: "Canyonlands National Park",
+      location: "Utah",
+      long_lat: [38.2136, 109.9025]
+    },
+    {
+      name: "Capitol Reef National Park",
+      location: "Utah",
+      long_lat: [38.0877, 111.1355]
+    },
+    {
+      name: "Carlsbad Caverns National Park",
+      location: "New Mexico",
+      long_lat: [32.1370, 104.5438]
+    },
+    {
+      name: "Congaree National Park",
+      location: "South Carolina",
+      long_lat: [33.7919, 80.7490]
+    },
+    {
+      name: "Crater Lake National Park",
+      location: "Oregon",
+      long_lat: [42.9295, 122.1279]
+    },
+    {
+      name: "Cuyahoga Valley National Park",
+      location: "Ohio",
+      long_lat: [41.2808, 81.5678]
+    },
+    {
+      name: "Death Valley National Park",
+      location: "California, Nevada",
+      long_lat: [36.5054, 117.0794]
+    },
+    {
+      name: "Denali National Park",
+      location: "Alaska",
+      long_lat: [63.2828, 150.7894]
+    },
+    {
+      name: "Dry Tortugas National Park",
+      location: "Florida",
+      long_lat: [24.6285, 82.8732]
+    },
+    {
+      name: "Everglades National Park",
+      location: "Florida",
+      long_lat: [25.2866, 80.8987]
+    },
+    {
+      name: "Gates of the Arctic National Park",
+      location: "Alaska",
+      long_lat: [67.6855, 153.3241]
+    },
+    {
+      name: "Gateway Arch National Park",
+      location: "Missouri",
+      long_lat: [38.6249, 90.1859]
+    },
+    {
+      name: "Glacier National Park",
+      location: "Montana",
+      long_lat: [48.7596, 113.7870]
+    },
+    {
+      name: "Grand Canyon National Park",
+      location: "Arizona",
+      long_lat: [36.2679, 112.3535]
+    },
+    {
+      name: "Grand Teton National Park",
+      location: "Wyoming",
+      long_lat: [43.7904, 110.6818]
+    },
+    {
+      name: "Great Basin National Park",
+      location: "Nevada",
+      long_lat: [38.9300, 114.2634]
+    },
+    {
+      name: "Great Sand Dunes National Park",
+      location: "Colorado",
+      long_lat: [37.7658, 105.6236]
+    },
+    {
+      name: "Great Smoky Mountains National Park",
+      location: "North Carolina, Tennessee",
+      long_lat: [35.6131, 83.5532]
+    },
+    {
+      name: "Guadalupe Mountains National Park",
+      location: "Texas",
+      long_lat: [31.55, 104.52]
+    },
+    {
+      name: "Haleakala National Park",
+      location: "Hawaii",
+      long_lat: [20.7204, 156.1552]
+    },
+    {
+      name: "Hawaii Volcanoes National Park",
+      location: "Hawaii",
+      long_lat: [19.4964, 155.4662]
+    },
+    {
+      name: "Hot Springs National Park",
+      location: "Arkansas",
+      long_lat: [34.5269, 93.0691]
+    },
+    {
+      name: "Indiana Dunes National Park",
+      location: "Indiana",
+      long_lat: [41.6533, 87.0524]
+    },
+    {
+      name: "Isle Royale National Park",
+      location: "Michigan",
+      long_lat: [47.9959, 88.9093]
+    },
+    {
+      name: "Joshua Tree National Park",
+      location: "California",
+      long_lat: [33.8734, 115.9010]
+    },
+    {
+      name: "Katmai National Park",
+      location: "Alaska",
+      long_lat: [58.5862, 154.9698]
+    },
+    {
+      name: "Kenai Fjords National Park",
+      location: "Alaska",
+      long_lat: [59.8487, 150.1879]
+    },
+    {
+      name: "Kings Canyon National Park",
+      location: "California",
+      long_lat: [36.8879, 118.5551]
+    },
+    {
+      name: "Kobuk Valley National Park",
+      location: "Alaska",
+      long_lat: [67.3575, 159.2137]
+    },
+    {
+      name: "Lake Clark National Park",
+      location: "Alaska",
+      long_lat: [60.6242, 153.3058]
+    },
+    {
+      name: "Lassen Volcanic Park",
+      location: "California",
+      long_lat: [40.4977, 121.4207]
+    },
+    {
+      name: "Mammoth Cave Park",
+      location: "Kentucky",
+      long_lat: [37.1815, 86.1505]
+    },
+    {
+      name: "Mesa Verde National Park",
+      location: "Colorado",
+      long_lat: [37.2309, 108.4618]
+    },
+    {
+      name: "Mount Rainier National Park",
+      location: "Washington",
+      long_lat: [46.8800, 121.7269]
+    },
+    {
+      name: "North Cascades National Park",
+      location: "Washington",
+      long_lat: [48.7718, 121.2985]
+    },
+    {
+      name: "Olympic National Park",
+      location: "Washington",
+      long_lat: [47.8021, 123.6044]
+    },
+    {
+      name: "Petrified Forest National Park",
+      location: "Arizona",
+      long_lat: [34.9583, 109.7594]
+    },
+    {
+      name: "Pinnacles National Park",
+      location: "California",
+      long_lat: [36.4906, 121.1825]
+    },
+    {
+      name: "Redwood National Park",
+      location: "California",
+      long_lat: [41.4017, 124.0417]
+    },
+    {
+      name: "Rocky Mountain National Park",
+      location: "Colorado",
+      long_lat: [40.3428, 105.6836]
+    },
+    {
+      name: "Saguaro National Park",
+      location: "Arizona",
+      long_lat: [32.1831, 110.6127]
+    },
+    {
+      name: "Sequoia National Park",
+      location: "California",
+      long_lat: [36.4864, 118.5658]
+    },
+    {
+      name: "Shenandoah National Park",
+      location: "Virginia",
+      long_lat: [38.4755, 78.4535]
+    },
+    {
+      name: "Theodore Roosevelt National Park",
+      location: "North Dakota",
+      long_lat: [46.9790, 103.5387]
+    },
+    {
+      name: "Virgin Islands National Park",
+      location: "U.S. Virgin Islands",
+      long_lat: [18.3435, 64.7501]
+    },
+    {
+      name: "Voyageurs National Park",
+      location: "Minnesota",
+      long_lat: [48.4711, 92.8458]
+    },
+    {
+      name: "Wind Cave National Park",
+      location: "South Dakota",
+      long_lat: [43.5724, 103.4416]
+    },
+    {
+      name: "Wrangell-St. Elias National Park",
+      location: "Alaska",
+      long_lat: [60.9234, 142.7339]
+    },
+    {
+      name: "Yellowstone National Park",
+      location: "Wyoming",
+      long_lat: [44.4280, 110.5885]
+    },
+    {
+      name: "Yosemite National Park",
+      location: "California",
+      long_lat: [37.8651, 119.5383]
+    },
+    {
+      name: "Zion National Park",
+      location: "Utah",
+      long_lat: [37.2982, 113.0263]
+    }
+  ];
 import { API_KEY } from './config.js';  
 var url = 'https://supercharge.info/service/supercharge/allSites'; 
 var gpsLayer; 
@@ -40,7 +338,7 @@ d3.json(url).then(function(response) {
                 opacity: 0.5
             });
             // Bind a popup to the circleMarker
-            circleMarker.bindPopup(`<strong>Place: </strong> ${site.name}<br><strong>Stall Count: </strong>${site.stallCount}`);
+            circleMarker.bindTooltip(`<strong>Place: </strong> ${site.name}<br><strong>Stall Count: </strong>${site.stallCount}`);
             // Add the circleMarker to the gpsMarkers array
             gpsMarkers.push(circleMarker);     
         });
@@ -107,7 +405,7 @@ let myMap = L.map("map", {
     var legend = L.control({position: 'bottomright'});
     legend.onAdd = function() {
         var div = L.DomUtil.create('div', 'info legend');
-        var stall_count = [ 0, 3, 9, 12, 24, 36];
+        var stall_count = [1, 6, 9, 12, 24, 36];
         var labels = [];
         var legendInfo = "<h3>Stall Count</h3>";
         div.innerHTML = legendInfo;
@@ -125,15 +423,23 @@ let myMap = L.map("map", {
         return stall_count * 200;
     }
 function scColor(stall_count) {
-    if (stall_count <= 3) {
+    if (stall_count <= 6) {
         return '#a7fb09'
-    } else if (stall_count <= 6) {
-        return '#dcf900'
     } else if (stall_count <= 9) {
-        return '#f6de1a'
+        return '#dcf900'
     } else if (stall_count <= 12) {
+        return '#f6de1a'
+    } else if (stall_count <= 16) {
         return '#f69a1a'
+    } else if (stall_count <= 20) {
+        return '#f66a1a'
     } else if (stall_count <= 24) {
-        return '#f64a1a'
+        return '#f63a1a'
+    } else if (stall_count <= 28) {
+        return '#f60a1a'
+    } else if (stall_count <= 32) {
+        return '#f6005a'
+    } else if (stall_count <= 36) {
+        return '#f6008a'
     } else { return '#800080' } 
 }
