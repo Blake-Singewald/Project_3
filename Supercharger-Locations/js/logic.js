7bbc0e0d37aa6e449a0ba480d84fe000e8477eaf
James Montgomery <145395275+braxtol@users.noreply.github.com>
2024-04-08 12:35:13 -0700

    Update logic.js

--- Supercharger-Locations/js/logic.js
+++ Supercharger-Locations/js/logic.js
@@ -19,7 +19,7 @@ var baseMaps = {
     "Topographic Map": topo 
 };  
 // Load National Parks data from CSV
-d3.csv('/Project_3/Archive/national_parks.csv').then(function(data) {
+d3.csv('/Project_3]/Archive/national_parks.csv').then(function(data) {
   data.forEach(function(park) {
     var marker = L.marker([park.lat, park.lng]);
     marker.bindPopup(`<b>${park.name}</b><br>${park.location}`);
@@ -27,7 +27,6 @@ d3.csv('/Project_3/Archive/national_parks.csv').then(function(data) {
     NationalParksLayer.addLayer(marker); // Add the marker to NationalParksLayer
   });
 });
-
 // Fetch data from the API using d3.json 
 d3.json(url).then(function(response) {     
    // console.log(response); // Check the response data
@@ -55,105 +54,18 @@ d3.json(url).then(function(response) {
         gpsMarkers.forEach(function(marker) {
             gpsLayer.addLayer(marker); // Add each circleMarker to gpsLayer
         });
-        // Define overlayMaps with the desired overlay layers and set 'checked' to true for the layers you want to be displayed by default
-        var overlayMaps = {
-            "SuperChargers": gpsLayer,
-            "National Parks": NationalParksLayer
-        };
-        // Create the Layer Control with the baseMaps, overlayMaps, and position it in the middle right corner
-        L.control.layers(baseMaps, overlayMaps, { position: 'topright' }).addTo(myMap);
+        var overlayMaps = {     
+            "SuperChargers": gpsLayer, // Use the gpsLayer variable defined inside the d3.json block
+            "National Parks": NationalParksLayer // Add NationalParksLayer to overlayMaps after it's assigned
+        }; 
+        L.control.layers(baseMaps, overlayMaps).addTo(myMap);
     } else {
         console.log("Response data is not in the expected format or is empty.");
     }
-});
+});  
 // Create the map and add Layer Control 
 let myMap = L.map("map", {     
     center: [37.09, -95.71],     
     zoom: 5,     
-    layers: [street] // Set the default base layer 
+    layers: [topo] // Set the default base layer 
 });  
-
-// function createMap(supercharge) {
-//     var centerCoordinates = [30.0902, 0];
-//     var mapZoom = 2.2;
-//     var satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//         tileSize: 512,
-//         maxZoom: 18,
-//         zoomOffset: -1,
-//         id: 'mapbox/satellite-v9',
-//         accessToken: API_KEY
-//     });
-//     var greyscale = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//         tileSize: 512,
-//         maxZoom: 18,
-//         zoomOffset: -1,
-//         id: 'mapbox/light-v10',
-//         accessToken: API_KEY
-//     }); 
-//     var outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//         tileSize: 512,
-//         maxZoom: 18,
-//         zoomOffset: -1,
-//         id: 'mapbox/outdoors-v11',
-//         accessToken: API_KEY
-//     });
-//     var baseMaps = {
-//         'Satellite': satellite,
-//         'Greyscale': greyscale,
-//         'Outdoors': outdoors
-//     };
-//     var overlayMaps = {
-//         'Super Chargers': supercharge
-//     };
-//     var myMap = L.map('map', {
-//         center: centerCoordinates,
-//         zoom: mapZoom,
-//         layers: [satellite, supercharge]
-//     });
-//     L.control.layers(baseMaps, overlayMaps, {
-//         collapsed: false
-//     }).addTo(myMap); 
-    var legend = L.control({position: 'bottomright'});
-    legend.onAdd = function() {
-        var div = L.DomUtil.create('div', 'info legend');
-        var stall_count = [1, 6, 9, 12, 24, 36];
-        var labels = [];
-        var legendInfo = "<h3>Stall Count</h3>";
-        div.innerHTML = legendInfo;
-        for (var i = 0; i < stall_count.length; i++) {
-            labels.push('<li style="background-color:' + scColor(stall_count[i] + 1) + '"> <span>' + stall_count[i] + (stall_count[i + 1] ? '&ndash;' + stall_count[i + 1] + '' : '+') + '</span></li>');
-        }
-        div.innerHTML += '<ul>' + labels.join('') + '</ul>';
-        
-        return div;
-    };
-    legend.addTo(myMap);
-
-    function markerSize(stall_count) {
-        // Multiply the stall count by 200 to make the radius 200 times bigger
-        return stall_count * 200;
-    }
-function scColor(stall_count) {
-    if (stall_count <= 6) {
-        return '#a7fb09'
-    } else if (stall_count <= 9) {
-        return '#dcf900'
-    } else if (stall_count <= 12) {
-        return '#f6de1a'
-    } else if (stall_count <= 16) {
-        return '#f69a1a'
-    } else if (stall_count <= 20) {
-        return '#f66a1a'
-    } else if (stall_count <= 24) {
-        return '#f63a1a'
-    } else if (stall_count <= 28) {
-        return '#f60a1a'
-    } else if (stall_count <= 32) {
-        return '#f6005a'
-    } else if (stall_count <= 36) {
-        return '#f6008a'
-    } else { return '#800080' } 
-}
