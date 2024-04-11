6e5534173427e662528fc34c98a57263d300cc54
James Montgomery <145395275+braxtol@users.noreply.github.com>
2024-04-11 00:02:37 -0700

    Update logic.js

--- Supercharger-Locations/js/logic.js
+++ Supercharger-Locations/js/logic.js
@@ -112,25 +112,25 @@ myMap.on('click', function(e) {
 });
 
 function updateSiteInfo(siteName, stallCount) {
-    if (!userSiteInfo) {
+    if (userSiteInfo) {
+        userSiteInfo.getContainer().innerHTML = "<h3>Charger Within Range</h3>" +
+            "<p>Name: " + siteName + "</p>" +
+            "<p>Available Stalls: " + stallCount + "</p>";
+    } else {
         userSiteInfo = L.control({ position: 'bottomleft' });
         userSiteInfo.onAdd = function() {
             var div = L.DomUtil.create('div', 'site info');
-            div.style.backgroundColor = 'white';
+            div.style.backgroundColor = 'white'; // Add white background color
             div.innerHTML = "<h3>Charger Within Range</h3>" +
                 "<p>Name: " + siteName + "</p>" +
                 "<p>Available Stalls: " + stallCount + "</p>";
             return div;
         };
         userSiteInfo.addTo(myMap);
-    } else {
-        userSiteInfo.getContainer().innerHTML = "<h3>Charger Within Range</h3>" +
-            "<p>Name: " + siteName + "</p>" +
-            "<p>Available Stalls: " + stallCount + "</p>";
     }
 }
 // Add click event listener to the map
-myMap.on('click', onMapClick);
+// myMap.on('click', onMapClick);
 
 function clearUserSelections() {
     clickedPoint.clearLayers();
@@ -183,3 +183,42 @@ function scColor(stall_count) {
     } else { return '#800080' } 
 }
 
+// Function to calculate distance between two points
+function calculateDistance(point1, point2) {
+    var distanceKm = point1.distanceTo(point2) / 1000; // Distance in kilometers
+    var distanceMiles = distanceKm * 0.621371; // Convert kilometers to miles
+    return distanceMiles;
+}
+
+// Function to handle click events on the map
+function onMapClick(e) {
+    if (!startPoint) {
+        startPoint = e.latlng;
+        
+        // Add a bright yellow marker at the start point
+        var startMarker = L.marker(startPoint, {
+            icon: L.icon({
+                iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png', // You can use a custom icon or change the color here
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+            })
+        }).addTo(myMap);
+
+        alert("Start point set!");
+    } else if (!endPoint) {
+        endPoint = e.latlng;
+        alert("End point set!");
+
+        // Calculate distance between two points
+        var distance = calculateDistance(startPoint, endPoint);
+        alert("Distance between points: " + distance.toFixed(2) + " miles");
+
+        // Reset points for the next calculation
+        startPoint = null;
+        endPoint = null;
+    }
+}
+// Add click event listener to the map
+myMap.on('click', onMapClick);
